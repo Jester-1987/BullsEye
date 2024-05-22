7c9e8a0eedeaba69844d6f1af919fe6731eb0e3c
Jester-1987 <nicole.groeger@triosstudent.com>
2024-05-22 11:07:56 -0400

    finished UI elements

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -8,26 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var roundLabel: UILabel!
+    
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewRound()
     }
 
     @IBAction func showAlert(){
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+        
+        let message = "You scored \(points) points"
+        
         let alert = UIAlertController(
-            title: "Hello, World", // shows title on popup
-            message: "This is my first app!", // main popup text
+            title: "Hello, World",
+            message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "Awesome", // action button text
+            title: "OK", // action button text
             style: .default,
             handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
     }
 
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
 }
 
