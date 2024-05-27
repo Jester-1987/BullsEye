366e300f02354f5a44281c3fc9e8bf7b4ab3b8eb
Jester-1987 <nicole.groeger@triosstudent.com>
2024-05-27 13:31:01 -0400

    finished about

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Nicole Groeger on 2024-05-27.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+  }
+}
+
