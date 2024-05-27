fdf2c98f9b949451290f019baec00bb0b2a6a48a
Jester-1987 <nicole.groeger@triosstudent.com>
2024-05-27 11:50:35 -0400

    updated scorekeeping/messages/newgame

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -26,25 +26,43 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert(){
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
+        var points = 100 - difference
+                
+        let title: String
+        if difference == 0 { // if the user gets to the exact target
+            title = "Perfect!"
+            points += 100
+        }
+        else if difference < 5 { // if the difference is less than 5 away from the target
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        }
+        else if difference < 10 { // if the difference is less than 10 away from the target
+            title = "Pretty Good!"
+        }
+        else {
+            title = "Not even close..." // if the user is too far from the target
+        }
         score += points
         
         let message = "You scored \(points) points"
         
         let alert = UIAlertController(
-            title: "Hello, World",
+            title: title,
             message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
             title: "OK", // action button text
             style: .default,
-            handler: nil)
+            handler: { _ in
+                self.startNewRound()
+            })
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        
-        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
